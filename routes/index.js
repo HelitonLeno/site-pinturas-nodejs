b5cc3ec82d9f59781d4b78aea2b8752f438c5950
HelitonLeno <ciencia_@live.com>
2018-01-30 20:44:35 -0200

    alterando nome site

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Ton&Leno | Pinturas' });
+  res.render('index', { 
+    title: 'Ton&Leno | Pinturas',
+    titleH1:'Ton%Leno' 
+  });
 });
 
 module.exports = router;
