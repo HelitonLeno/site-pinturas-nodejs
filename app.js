2529f81a746a0509749b72d9b5781d4fc3c17e5e
HelitonLeno <ciencia_@live.com>
2018-02-13 17:55:27 -0200

    bug http

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.enable('trust proxy');
 
 app.use('/', index);
 app.use('/simulador', simulador);
