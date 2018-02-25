ed2ab02ab8fc2a18fb4a9c8b6628e0c9bf8df057
HelitonLeno <ciencia_@live.com>
2018-02-25 18:22:52 -0300

    arrumando arquivos

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ const parceiros = require('./routes/parceiros');
 
 const app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -31,7 +30,6 @@ app.use('/cores', cores);
 app.use('/parceiros', parceiros);
 app.use('/simulador', simulador);
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
