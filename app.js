f4a11bec925d979db12aefcae828706e8da487b9
HelitonLeno <ciencia_@live.com>
2018-02-25 17:13:30 -0300

    implementando area parceiros

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/cores', cores);
-app.use('/simulador', simulador);
 app.use('/parceiros', parceiros);
+app.use('/simulador', simulador);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
